23a00ff697a1de843d41fd7750e5ef2bad927968
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2022-12-05 16:39:17 +0530

    Showing Welcome msg

---
+++ WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day3employeewageproblem;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage program");
+    }
+}
